f5d9828d856e85a88ae8e862dfc99aa920063880
cheddar-design <itslegoman823@gmail.com>
2023-11-03 11:48:12 -0500

    init

--- app.js
+++ app.js
@@ -9,54 +9,47 @@
  * Released under the terms of the Affero GPL v3
  */
 
-var url = require('url');
-var querystring = require('querystring');
-var express = require('express');
-var Unblocker = require('unblocker');
-var Transform = require('stream').Transform;
-var youtube = require('unblocker/examples/youtube/youtube.js')
+var url = require("url");
+var querystring = require("querystring");
+var express = require("express");
+var Unblocker = require("unblocker");
+var Transform = require("stream").Transform;
+var youtube = require("unblocker/examples/youtube/youtube.js");
+
+const replaceSnippet = require("./snippet.js");
 
 var app = express();
 
-function editTitle(data) {
-    console.log(data);
-    if (data.contentType == 'text/html') {
-        var myStream = new Transform({
-            decodeStrings: false,
-            function(chunk, encoding, next) {
-                chunk = chunk.slice(0,chunk.search(/<title>/g)) + '<title>Cheddar Design' + chunk.slice(chunk.search(/<\/title>/g))
-                this.push(chunk);
-                next();
-                }
-        });
-        data.stream = data.stream.pipe(myStream);
-    }
-}
-
 var unblocker = new Unblocker({
-    prefix: '/hdg/',
-    requestMiddleware: [
-        youtube.processRequest
-    ],
-    responseMiddleware: [
-        editTitle
-    ],
+  prefix: "/hdg/",
+  requestMiddleware: [youtube.processRequest],
+  responseMiddleware: [
+    replaceSnippet({
+      processContentTypes: ["text/html"],
+      searchFor: /<title>.*<\/title>/i,
+      replaceWith: "<title>Hello</title>",
+    }),
+    replaceSnippet({
+      processContentTypes: ["text/html"],
+      searchFor: /<link rel="shortcut icon".*>/i,
+      replaceWith:
+        '<link rel="shortcut icon" href="https://simplebits.com/apple-touch-icon.png">',
+    }),
+  ],
 });
 
 // this line must appear before any express.static calls (or anything else that sends responses)
 app.use(unblocker);
 
 // serve up static files *after* the proxy is run
-app.use('/', express.static(__dirname + '/public'));
+app.use("/", express.static(__dirname + "/public"));
 
 // this is for users who's form actually submitted due to JS being disabled or whatever
-app.get("/no-js", function(req, res) {
-    // grab the "url" parameter from the querystring
-    var site = querystring.parse(url.parse(req.url).query).url;
-    // and redirect the user to /proxy/url
-    res.redirect(unblockerConfig.prefix + site);
+app.get("/no-js", function (req, res) {
+  // grab the "url" parameter from the querystring
+  var site = querystring.parse(url.parse(req.url).query).url;
+  // and redirect the user to /proxy/url
+  res.redirect(unblockerConfig.prefix + site);
 });
 
-app.listen(8080, function() {
-    console.log(`node unblocker process listening at http://localhost:${port}/`);
-}).on("upgrade", unblocker.onUpgrade); // onUpgrade handles websockets
+app.listen(8080, function () {});
